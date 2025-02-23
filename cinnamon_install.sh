96db460fdd284d93ab2b063f9c4aa3ce40191203
Daniel <161472768+TechGameGeek@users.noreply.github.com>
2025-02-23 19:56:00 +0100

    Update cinnamon_install.sh

--- cinnamon_install.sh
+++ cinnamon_install.sh
@@ -1,18 +1,50 @@
 #!bin/bash
+
+#Void aktualisieren / update void
 sudo xbps-install -Syu
+
+#void-nonfree Repository aktivieren / Activate void nonfree-repository
 sudo xbps-install -y void-repo-nonfree
+
+#void-multilib Repository aktivieren / Activate void multilib-repository (e.g. for Steaminstall)
 sudo xbps-install -y void-repo-multilib
+
+#Voidrepo aktualisieren / update voidrepository
 sudo xbps-install -Syu
-sudo xbps-install -y nvidia-driver nvidia-drm nvidia-utils
-sudo xbps-install -y xorg xorg-server xf86-input-libinput
-sudo xbps-install -y cinnamon cinnamon-desktop cinnamon-control-center cinnamon-screensaver cinnamon-settings-daemon cinnamon-applets cinnamon desktlets gnome-themes 
-sudo xbps-install -y papirus-icon-theme sudo xbps-install -y cinnamon-themes sddm cinnamon-themes
-sudo ln -s /etc/sv/sddm /var/service/
-sudo xbps-install -y firefox-esr vlc gstreamer gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-ugly gnome-system-monitor xrandr dconf-editor
-sudo xbps-install -y  
-xbps-install -y network-manager
-xbps-install -y network-manager-applet
-sudo ln -s /etc/sv/networkmanager /var/service/
-sudo xbps-install -y pipewire pipewire-pulse pavucontrol
+
+#Netzwerk/Network
+sudo xbps-install -y NetworkManager
+sudo ln -s /etc/sv/NetworkManager /var/service/
+
+#Audio
+sudo xbps-install -y pipewire wireplumber
 sudo ln -s /etc/sv/pipewire /var/service/
 sudo ln -s /etc/sv/pipewire-pulse /var/service/
+sudo usermod -aG _pipewire,pulse,pulse-access $USER
+
+#dbus
+sudo xbps-install -y dbus
+sudo ln -s /etc/sv/dbus /var/service
+
+
+#NVIDIA Treiber installieren / Install NVIDIA-driver
+sudo xbps-install -y nvidia-driver nvidia-drm nvidia-utils
+
+#XORG & Cinnamon & Tools
+sudo xbps-install -y xorg
+sudo xbps-install -y cinnamon-all xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils
+
+#Druckerunterstuetzung / Printersupport
+sudo xbps-install -y cups cups-filters gutenprint
+sudo ln -s /etc/sv/cupsd /var/service
+
+#Fonts
+sudo xbps-install -y noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra
+
+#Software
+sudo xbps-install -y firefox-esr terminal
+
+#Loginmanager
+sudo xbps-install -y lightdm light-gtk-greeter
+sudo ln -s /etc/sv/lightdm/ /var/service/
+

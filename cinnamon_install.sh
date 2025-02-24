49ca13ffbd7c3e93af9ee79dc204db7f820240be
Daniel <161472768+TechGameGeek@users.noreply.github.com>
2025-02-24 16:17:39 +0100

    Update cinnamon_install.sh

--- cinnamon_install.sh
+++ cinnamon_install.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 #Skript ist noch nicht fertig - noch nicht verweden! / WORK IN PROGRESS -- do not use yet!
 #Das Skript ist für NVIDIA-GPU Besitzer gedacht / AMD GPU Besitzer kommentieren die Zeile mit der NVIDIA Paketinstallation aus!
 #This script is intended to be used by NVIDIA-GPU Owners, you may comment the line with

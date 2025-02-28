dd987466ba1c027f25e688219bd0b38f7bc6b6ad
Daniel <161472768+TechGameGeek@users.noreply.github.com>
2025-02-28 07:24:47 +0100

    Update cinnamon_install.sh

--- cinnamon_install.sh
+++ cinnamon_install.sh
@@ -156,7 +156,7 @@ sleep 1
 #Software
 clear
 echo "Install Software..."
-sudo xbps-install -y firefox gnome-terminal
+sudo xbps-install -y firefox gnome-terminal firefox-i18n-de
 sleep 1
 # Erstelle ein Skript, das die gsettings nach der Anmeldung ausführt
 echo "Creating autostart script for cinnamon theme settings..."

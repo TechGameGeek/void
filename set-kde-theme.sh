ba19b67f38e6f250795bfa7ac0ec0ef935ba9e18
Daniel <161472768+TechGameGeek@users.noreply.github.com>
2025-03-02 12:51:43 +0100

    Update set-kde-theme.sh

--- set-kde-theme.sh
+++ set-kde-theme.sh
@@ -3,9 +3,6 @@
 plasma-apply-desktoptheme breeze-dark
 lookandfeeltool -a org.kde.breezedark.desktop
 
-#Hintergrundbild festlegen / set wallpaperimage
-plasma-apply-wallpaperimage "/usr/share/backgrounds/background.jpg"
-
 # Lösche den Autostart-Eintrag nach der ersten Ausführung / remove one-time-autostart-entry
 rm -f ~/.config/autostart/set-kde-theme.desktop
 
